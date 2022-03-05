a5f367f634ff4e74290416f01075c6d3715cfed4
Bogdan Fuia <bogdan.fuia@gmail.com>
2022-03-05 14:39:07 +0200

    implemeneted endpoint for disk usage

--- api.py
+++ api.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
 
+import subprocess
+import re
+
 api = FastAPI()
 
 
 @api.get("/")
 def read_root():
-    return "Hello Server World!"
+    return "Hello World!"
+
+
+@api.get("/disk")
+def read_disk_info():
+
+    output = subprocess.Popen(
+        ["df", "-h"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    stdout, _ = output.communicate()
+
+    stdout = re.sub("[ ]+", " ", stdout.decode())
+    info_line = stdout.split("\n")[1]
+    info_list = info_line.split(" ")
+    system_info_headlines = [
+        "filesystem",
+        "size",
+        "used",
+        "available",
+        "usage",
+        "mounted on",
+    ]
+
+    info = {key: value for key, value in zip(system_info_headlines, info_list)}
+
+    return info
